37a3be738f1d328e30d73ad5a23c5ce47f8985aa
Mr-76 <bartmoss77@tutanota.com>
2025-02-15 19:52:57 -0300

    adding a descp to a task working....

--- task_tracker.nim
+++ task_tracker.nim
@@ -1,11 +1,16 @@
 import std/[times, os]
 import std/json
 import std/files
+import std/strutils
 
 var
   arguments = commandLineParams()
 sleep(500) # Replace this with something to be timed
 
+
+proc load_task(): string = 
+   let result = if not fileExists("./tasks.json"): "false" else: readFile("tasks.json")
+   result
 
 proc add_task(task_name:string) =
   if not fileExists("./tasks.json"):
@@ -21,10 +26,13 @@ proc add_task(task_name:string) =
     writeFile("tasks.json",$jsonNode)
 
 
-
-proc add_desc(task_name:string,descp:string) =
- echo "hello"
-
+proc add_desc(task_name: string, descp: string) =
+  var jsonFile = load_task()
+  var JsonNode = parseJson(jsonFile)
+  var node = JsonNode[task_name]
+  node["task_descp"] = %descp
+  JsonNode[task_name] = node  
+  writeFile("tasks.json", $JsonNode)
 
 proc set_due(task_name:string,date:string) =
  echo "hello"
@@ -37,6 +45,9 @@ proc set_daily(task_name:string) =
  
 proc cli() = 
  for arg in arguments:
+
+
+   echo len(arguments)
    case arg:
      of "--add-task":
        echo("add a task")
@@ -45,8 +56,10 @@ proc cli() =
      of "--add-desc":
        echo("add a task")
        let task_name = arguments[1]
-       let task_desc = arguments[2]
-       add_desc(task_name,task_desc)
+       #let task_des = arg
+       let task_desc = arguments[2 .. len(arguments) - 1]
+       var descp = task_desc.join(" ")
+       add_desc(task_name,descp)
      of "--add-due-date":
       echo("add a task")
       let task_name = arguments[1]
@@ -63,3 +76,6 @@ proc cli() =
  
 
 cli()
+
+#why not just load every thing in a struck the tasks and go with it ......
+#why use the whole file if just modifying one task for example...
